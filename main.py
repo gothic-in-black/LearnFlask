07508257270e0aac1b1a6b3a0477d886887b2cfa
Natalia <gothic-in-black@mail.ru>
2023-05-31 16:30:25 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+#                                                      проект 1 неделя
+# from flask import Flask, render_template
+#
+# app = Flask(__name__)
+#
+# @app.route('/')
+# def main():
+#     return render_template('index.html')
+#
+# @app.route('/departures/<departure>/')
+# def departure():
+#     return render_template('departure.html')
+#
+# @app.route('/tours/<id>/')
+# def tour():
+#     return render_template('tour.html')
+#
+# @app.route('/data')
+# def all_tours():
+#     return  render_template('data.html')
+#
+# @app.route('/data/departures/<departure>')
+# def city_from():
+#     return render_template('from.html')
+#
+# @app.route('/data/tours')
+# def hotels():
+#     return render_template('hotels.html')
+#
+#
+#
+# app.run()
+# if __name__ == '__main__':
+#     app.run()
